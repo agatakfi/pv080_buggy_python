19b8b3e5e093b4a72c71835479aa8a8841ffe436
agatakfi <adarbuja@redhat.com>
2022-04-26 22:06:55 +0200

    App fixed

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,11 +1,12 @@
 # contains bunch of buggy examples
 # taken from https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
-from app import app
+
 
 import subprocess
 import base64
 import pickle
 import flask
+from app import app
 
 # Input injection
 def transcode_file(filename):
